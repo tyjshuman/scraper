266bbf85f5235a21cb008585ce40512d32d279a5
Zachary Keeton <zachary.keeton@gmail.com>
2017-08-26 13:22:48 -0500

    fixed typo and indentation mess ups from gedit

--- virtuance_scraper/virtuance_scraper/spiders/virtuance_spider.py
+++ virtuance_scraper/virtuance_scraper/spiders/virtuance_spider.py
@@ -14,7 +14,7 @@ class VirtuanceSpider(scrapy.Spider):
 		end_page_number 		= getattr(self, 'e', '1')
 		num_results_per_page 	= getattr(self, 'n', '10')
 	
-		for page in range(int(start_page_number, int(end_page_number) + 1):
+		for page in range(int(start_page_number), int(end_page_number) + 1):
 		
 			page_url = "http://tours.virtuance.com/public/tour/search/WFNull/%s|%s|" % (page, num_results_per_page)
 			logging.info("Scraping page: " + page_url)
@@ -35,11 +35,11 @@ class VirtuanceSpider(scrapy.Spider):
 			
 				contact = response.css('div[id=agentInfoInner]')[0]
 				name 	= contact.css('span[id=customerFullName]::text').extract_first()
-       			company = contact.css('span[id=customerCompany]::text').extract_first()
-		        phone 	= contact.css('span[id=customerContactPhone]::text').extract_first()
-     	        email 	= contact.css('a[id=customerEmail]::text').extract_first()
+       				company = contact.css('span[id=customerCompany]::text').extract_first()
+		        	phone 	= contact.css('span[id=customerContactPhone]::text').extract_first()
+     	        		email 	= contact.css('a[id=customerEmail]::text').extract_first()
      	        
-				if name is None:
+     	        		if name is None:
 					logging.error("Missed name at: " + page_url)
 					
 				else:
