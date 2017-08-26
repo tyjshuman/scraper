36d7e3f53d3a9e43700007b69147ec7d25553137
Zachary Keeton <zachary.keeton@gmail.com>
2017-08-26 12:41:14 -0500

    added support for command line page numbers

--- virtuance_scraper/virtuance_scraper/spiders/virtuance_spider.py
+++ virtuance_scraper/virtuance_scraper/spiders/virtuance_spider.py
@@ -1,17 +1,25 @@
 import scrapy
 import json
+import logging
 
 class VirtuanceSpider(scrapy.Spider):
+
 	name = 'virtuance'
-	def start_requests(self):
-		urls = ['http://tours.virtuance.com/public/tour/search']
 	
-		for page in range(70, 81):
-			print "Scraping page: " + str(page)
-			page_url = urls[0]+"/WFNull/"+ str(page) +"|1000|"
+	def start_requests(self):
+	
+		start_page_number 		= getattr(self, 's', '1') # using string numbers since command line args come in as strings.
+		end_page_number 		= getattr(self, 'e', '1')
+		num_results_per_page 	= getattr(self, 'n', '10')
+	
+		for page in range(int(start_page_number, int(end_page_number) + 1):
+		
+			page_url = "http://tours.virtuance.com/public/tour/search/WFNull/%s|%s|" % (page, num_results_per_page)
+			logging.info("Scraping page: " + page_url)
 			yield scrapy.Request(url=page_url, callback=self.parse)
 
 	def parse(self, response):
+	
 		#Iterate through the listings on the page
 		if response.css('title::text').extract_first() == "Tours hosted by tours.virtuance.com":
 			for links in response.xpath('.//a[contains(@id,"viewBranded")]'):
