ecb2744b9c538085e9a027c8aebf1ebf034b2652
ty <tyjshuman@gmail.com>
2017-08-27 15:20:57 -0500

    initial commit for mongo shiz

---
+++ mongomongomongo/README.txt
@@ -0,0 +1,1 @@
+This directory will hold everything used to populate a database using pymongo from the output of the virtuance scraper.
--- virtuance_scraper/virtuance_scraper/settings.py
+++ virtuance_scraper/virtuance_scraper/settings.py
@@ -22,7 +22,7 @@ USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
