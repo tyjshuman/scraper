c74c0651b396ab81dd3e45c6dbedd3e2814ef5cb
ty <tyjshuman@gmail.com>
2017-09-24 14:13:43 -0500

    updated

--- mongomongomongo/pymongoShit/writeToCSV.py
+++ mongomongomongo/pymongoShit/writeToCSV.py
@@ -2,12 +2,13 @@ import pymongo
 import csv
 import re
 from pymongo import MongoClient
+#remove duplicates db.newClients.find({}, {email:1}).sort({_id:1}).forEach(function(doc){     db.newClients.remove({_id:{$gt:doc._id}, email:doc.email}); })
 
 #Load the database
 mongo = MongoClient("mongodb://localhost:27017")
 db = mongo["virtuance"]
 
-cursor = db.clients.find({"address" : {'$regex' : ".*TX.*"}},{'_id':0, 'name':1, 'company':1, 'email':1, 'address':1})
+cursor = db.newClients.find({"address" : {'$regex' : ".*TX.*"}},{'_id':0, 'name':1, 'company':1, 'email':1, 'address':1})
 
 def validateClientInfo(client):
 	valid = True
@@ -49,7 +50,7 @@ def writeCSV(entries):
 						lastName = client["name"][client["name"].index(' ') + 1:]
 					elif client["name"].count(' ') is 2:
 						lastName = client["name"][client["name"].rfind(' ') + 1:]
-					writer.writerow({"first":firstName, "last":lastName, "email":client["email"], "broker":client["company"],"zip":re.search('[0-9]{5},',client['address']).group(0)})
+					writer.writerow({"first":firstName, "last":lastName, "email":client["email"], "broker":client["company"],"zip":re.search('[0-9]{5},',client['address']).group(0)[:-1]})
 				except:
 					print client
 
